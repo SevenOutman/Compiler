319a0abeae43e9dc40ff8543a2437617272b3e88
SevenOutman <499425729@qq.com>
2015-12-21 22:47:57 +0800

    增加了四则运算三地址

--- js/Semantic.js
+++ js/Semantic.js
@@ -15,9 +15,15 @@ function SemanticAnalyzer() {
         activeNUM = null;
 
     var _errors = [];
+    var _assembly = [],
+        _assemblyStack = [];
+    var _t = 0;
+
 
     var _eat = function (tree, st) {
+        _t = 0;
         _errors = [];
+        _assembly = [];
         var root = tree;
         symboltable = st;
         symboltable.get = function (name) {
@@ -28,14 +34,237 @@ function SemanticAnalyzer() {
             }
         };
         activeID = null;
-        _recursive(root);
+        //_recursive(root);
+        _r(root);
+        console.log(root);
+
         P("symboltablechanged", symboltable);
         //if (!!_errors.length) {
         //    P("semantichaserror", _errors);
         //}
     };
 
+    function l(type, ad1, ad2, ad3) {
+        var a1 = ad1 || "",
+            a2 = ad2 || "",
+            a3 = ad3 || "";
+        return type + " " + a1 + ", " + a2 + ", " + a3;
+    }
+
+    function _r(node) {
+        var father = nodeStack.rear();
+        nodeStack.push(node);
+        switch (node.abstract) {
+            case "program":
+            {
+                _r(node.subNodes[0]);
+                break;
+            }
+            case "compoundstmt":
+            {
+                _r(node.subNodes[1]);
+                break;
+            }
+            case "stmts":
+            {
+                _r(node.subNodes[0]);
+                if (node.subNodes[1]) {
+                    _r(node.subNodes[1]);
+                }
+                break;
+            }
+            case "stmt":
+            {
+                _r(node.subNodes[0]);
+                break;
+            }
+            case "decl":
+            {
+                node.value = _r(node.subNodes[0]).value;
+                _r(node.subNodes[1]);
+                break;
+            }
+            case "type":
+            {
+                node.value = node.subNodes[0].abstract;
+                break;
+            }
+            case "list":
+            {
+                node.value = father.value;
+                _r(node.subNodes[0]);
+                _r(node.subNodes[1]);
+                break;
+            }
+            case "ID":
+            {
+                var symbol = symboltable.get(node.name);
+                if (undefined === symbol.occurance) {
+                    symbol.occurance = 0;
+                } else {
+                    symbol.occurance++;
+                }
+                switch (father.abstract) {
+                    case "list":
+                    {
+                        node.type = father.value;
+                        if (symbol.type) {
+                            _errors.push(new SemanticError("Duplicated declaration of '" + symbol.name + "'", symbol.positions[symbol.occurance]));
+                        }
+                        symbol.type = node.type;
+                        break;
+                    }
+                }
+                break;
+            }
+            case "list1":
+            {
+                node.value = father.value;
+                if (node.subNodes[1]) {
+                    _r(node.subNodes[1]);
+                }
+                break;
+            }
+            case "assgstmt":
+            {
+                node.value = _r(node.subNodes[2]).value;
+                var id = _r(node.subNodes[0]).name;
+                _assembly.push(l("mov", id, null, node.value));
+                console.log(_assembly);
+                break;
+            }
+            case "arithexpr":
+            {
+                var first = _r(node.subNodes[0]),
+                    second = _r(node.subNodes[1]);
+                if (second.type) {
+                    switch (second.type) {
+                        case "+":
+                        {
+                            node.value = "t" + _t++;
+                            _assembly.push(l("add", node.value, first.value, second.value));
+                            break;
+                        }
+                        case "-":
+                        {
+                            node.value = "t" + _t++;
+                            _assembly.push(l("sub", node.value, first.value, second.value));
+                            break;
+                        }
+                    }
+                } else {
+                    node.value = first.value;
+                }
+                break;
+            }
+            case "multexpr":
+            {
+                var first = _r(node.subNodes[0]),
+                    second = _r(node.subNodes[1]);
+                if (second.type) {
+                    switch (second.type) {
+                        case "*":
+                        {
+                            node.value = "t" + _t++;
+                            _assembly.push(l("mul", node.value, first.value, second.value));
+                            break;
+                        }
+                        case "/":
+                        {
+                            node.value = "t" + _t++;
+                            _assembly.push(l("div", node.value, first.value, second.value));
+                            break;
+                        }
+                    }
+                } else {
+                    node.value = first.value;
+                }
+                break;
+            }
+            case "arithexprprime":
+            {
+                if (node.subNodes[2]) {
+                    node.type = node.subNodes[0].abstract;
+                    var first = _r(node.subNodes[1]),
+                        second = _r(node.subNodes[2]);
+                    if (second.type) {
+                        switch (second.type) {
+                            case "+":
+                            {
+                                node.value = "t" + _t++;
+                                _assembly.push(l("add", node.value, first.value, second.value));
+                                break;
+                            }
+                            case "-":
+                            {
+                                node.value = "t" + _t++;
+                                _assembly.push(l("sub", node.value, first.value, second.value));
+                                break;
+                            }
+                        }
+                    } else {
+                        node.value = first.value;
+                    }
+                }
+                break;
+            }
+            case "simpleexpr":
+            {
+                if (node.subNodes[1]) {
+                    node.value = _r(node.subNodes[1]).value;
+                } else {
+                    switch (node.subNodes[0].abstract) {
+                        case "ID":
+                        {
+                            node.value = node.subNodes[0].name;
+                            break;
+                        }
+                        case "NUM":
+                        {
+                            node.value = node.subNodes[0].value;
+                            break;
+                        }
+                    }
+                }
+                break;
+            }
+            case "multexprprime":
+            {
+                if (node.subNodes[2]) {
+                    node.type = node.subNodes[0].abstract;
+                    var first = _r(node.subNodes[1]),
+                        second = _r(node.subNodes[2]);
+                    if (second.type) {
+                        switch (second.type) {
+                            case "*":
+                            {
+                                node.value = "t" + _t++;
+                                _assembly.push(l("mult", node.value, first.value, second.value));
+                                break;
+                            }
+                            case "/":
+                            {
+                                node.value = "t" + _t++;
+                                _assembly.push(l("div", node.value, first.value, second.value));
+                                break;
+                            }
+                        }
+                    } else {
+                        node.value = first.value;
+                    }
+                }
+                break;
+            }
+            default:
+                break;
+        }
+        nodeStack.pop();
+        return node;
+    }
+
+
     function _recursive(node) {
+        console.log(node);
         if (node.abstract == "ID") {
             var symbol = symboltable.get(node.name);
             if (undefined === symbol.occurance) {
@@ -46,13 +275,18 @@ function SemanticAnalyzer() {
             symbolStack.push(symbol);
         } else if (node.abstract == "int" || node.abstract == "real") {
             activeType = node.abstract;
+        } else if (node.abstract == "NUM") {
+            NUMStack.push(node.value);
         }
 
-        if (node.abstract == ";") {
-            activeType = null;
-            NUMStack.pop();
-            symbolStack.pop();// activeID = activeNUM = null;
+        if (nodeStack.rear() && nodeStack.rear().abstract == "assgstmt") {
+            if (node.abstract == ";") {
+                var asbly = "mov " + symbolStack.rear().name + ", , " + NUMStack.rear();
+                _assembly.push(asbly);
+                console.log(_assembly);
+            }
         }
+
 
         // try declare
         if (nodeStack.rear() && nodeStack.rear().abstract == "list") {
@@ -73,6 +307,12 @@ function SemanticAnalyzer() {
                     _errors.push(new SemanticError("Undeclared identifier '" + symbol.name + "'", symbol.positions[symbol.occurance]));
                 }
             }
+        }
+
+        if (node.abstract == ";") {
+            activeType = null;
+            NUMStack.pop();
+            symbolStack.pop();
         }
 
         nodeStack.push(node);
